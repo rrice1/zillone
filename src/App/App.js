ff13f89660cd13079d9b6b7739ebf07e8d0585e6
Robert Rice <rrice1291@gmail.com>
2018-12-11 20:52:16 -0600

    login

--- src/App/App.js
+++ src/App/App.js
@@ -3,19 +3,34 @@ import React, { Component } from 'react';
 import connection from '../helpers/data/connection';
 
 import Auth from '../components/Auth/Auth';
-
+import Listings from '../components/Auth/Listings/Listings';
 import './App.scss';
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
   componentDidMount() {
     connection();
   }
 
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
-    console.log(this);
+    if (!this.state.authed) {
+      return (
+        <div className="App">
+          <Auth isAuthenticated={this.isAuthenticated}/>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <Auth />
+        <Listings/>
       </div>
     );
   }
--- src/helpers/data/authRequests.js
+++ src/helpers/data/authRequests.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const authenticate = () => {
-  const provider = new firebase.auth.GoogleAuthProvider(); 
+  const provider = new firebase.auth.GoogleAuthProvider();
   // for homework change GoogleAuthProvider to GitHubAuthProvider or something
   return firebase.auth().signInWithPopup(provider);
 };
