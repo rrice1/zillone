933725e5714b892bff4a21b4d859a42932a98f40
Robert Rice <rrice1291@gmail.com>
2018-12-11 20:22:40 -0600

    login

---
+++ src/helpers/data/authRequests.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const authenticate = () => {
+  const provider = new firebase.auth.GoogleAuthProvider(); 
+  // for homework change GoogleAuthProvider to GitHubAuthProvider or something
+  return firebase.auth().signInWithPopup(provider);
+};
+
+export default {
+  authenticate,
+};
