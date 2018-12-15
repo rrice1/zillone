cf155fd9a28b503e8c270eee01f8f88e07878573
Robert Rice <rrice1291@gmail.com>
2018-12-15 11:17:45 -0600

    listing styling

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -18,7 +18,7 @@ class Listings extends React.Component {
     return (
       <div className="listings col">
         <h2>Listings</h2>
-        {listingsItemComponents}
+        <ul>{listingsItemComponents}</ul>
       </div>
     );
   }
