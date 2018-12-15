2a10ba749b004f8c441fe656653ff5419f2ca2e2
Robert Rice <rrice1291@gmail.com>
2018-12-15 11:00:42 -0600

    listing data

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import listingShape from '../../helpers/propz/listingShape';
+import ListingItem from '../ListingItem/ListingItem';
 import './Listings.scss';
 
 
@@ -10,9 +11,14 @@ class Listings extends React.Component {
   }
 
   render() {
+    const { listings } = this.props;
+    const listingsItemComponents = listings.map(listing => (
+      <ListingItem key={listing.id} listing={listing}/>
+    ));
     return (
       <div className="listings col">
         <h2>Listings</h2>
+        {listingsItemComponents}
       </div>
     );
   }
