8d9a74c842495e923c2a213d4fabf9fd6ccb8cdc
Robert Rice <rrice1291@gmail.com>
2018-12-15 12:36:21 -0600

    delete

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import listingShape from '../../helpers/propz/listingShape';
 import formatPrice from '../../helpers/formatPrice';
@@ -9,33 +10,40 @@ import authRequests from '../../helpers/data/authRequests';
 class ListingItem extends React.Component {
   static propTypes = {
     listing: listingShape,
+    deleteSingleListing: PropTypes.func,
   }
 
-  render() {
-    const { listing } = this.props;
-    const uid = authRequests.getCurrentUid();
-    const makeButtons = () => {
-      if (listing.uid === uid) {
-        return (
+deleteEvent = (e) => {
+  e.preventDefault();
+  const { deleteSingleListing, listing } = this.props;
+  deleteSingleListing(listing.id);
+}
+
+render() {
+  const { listing } = this.props;
+  const uid = authRequests.getCurrentUid();
+  const makeButtons = () => {
+    if (listing.uid === uid) {
+      return (
           <div>
             <span className="col">
-            <button className="btn btn-default">
+            <button className="btn btn-default" onClick={this.deleteEvent}>
               <i className="fas fa-trash-alt"></i>
             </button>
             </span>
           </div>
-        );
-      }
-      return <span className="col-2"></span>;
-    };
-    return (
+      );
+    }
+    return <span className="col-2"></span>;
+  };
+  return (
 <li className="listing-item text-center">
 <span className="col-7">{listing.address}</span>
 <span className="col-3">{formatPrice(listing.price)}</span>
 {makeButtons()}
 </li>
-    );
-  }
+  );
+}
 }
 
 export default ListingItem;
